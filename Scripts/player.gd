482f93a86b0793c329338c1d98522dc1e0c0e485
Courtland Crouchet <CrouchetCourtland@gmail.com>
2025-02-16 22:05:07 -0500

    Initial Movement

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,5 +1,6 @@
 extends RigidBody2D
 
+var movement_force = 1000
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
@@ -9,3 +10,12 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	pass
+
+func _physics_process(delta):
+	var input = Vector2.ZERO
+	if Input.is_action_pressed("move_left"):
+		input.x -= 1
+	if Input.is_action_pressed("move_right"):
+		input.x += 1
+	
+	apply_force(input.normalized() * movement_force)
