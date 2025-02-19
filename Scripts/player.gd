4183ac4f7ccf7131fd405949c613f05386539301
Courtland Crouchet <CrouchetCourtland@gmail.com>
2025-02-18 21:57:44 -0500

    Fixed it :)

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -17,5 +17,13 @@ func _physics_process(delta):
 		input.x -= 1
 	if Input.is_action_pressed("move_right"):
 		input.x += 1
+		
+	#if Input.is_action_pressed("rotate_left"):
+		#$Arm.apply_torque(-1000)
+	#elif Input.is_action_pressed("rotate_right"):
+		#$Arm.apply_torque(1000)
+	#else:
+		#$Arm.angular_velocity *= 0.9
+		
 	
 	apply_force(input.normalized() * movement_force)
