3b0ba768bde4f63b6bd0c3cece92a6fe08b3f4cb
Courtland Crouchet <CrouchetCourtland@gmail.com>
2025-02-19 20:18:20 -0500

    Reset with P

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -3,9 +3,9 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
-
+	pass
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Input.is_action_just_pressed("reset_level"):
+		get_tree().reload_current_scene()
