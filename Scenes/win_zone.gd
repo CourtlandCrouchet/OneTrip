6b3e0bc25dbda10e04cbf79d849e5ca739122406
LukeDuhe <Luke7490@gmail.com>
2025-02-19 21:24:30 -0600

    add win zone

---
+++ Scenes/win_zone.gd
@@ -0,0 +1,19 @@
+extends Area2D
+
+var box_count = 0
+var end_level_timer = 3
+
+func _ready() -> void:
+	$Label.text = str(end_level_timer)
+
+func _on_body_entered(body: Node2D) -> void:
+	if body.is_in_group("Player"):
+		$Timer.start()
+	if body.is_in_group("Box"):
+		box_count += 1
+
+func _on_timer_timeout() -> void:
+	end_level_timer -= 1
+	$Label.text = str(end_level_timer)
+	if end_level_timer <= -1:
+		$Label.text = "Boxes Collected: " + str(box_count)
--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,6 +1,6 @@
 extends RigidBody2D
 
-var movement_force = 2000
+var movement_force = 1000
 const MIN_ANGLE = deg_to_rad(-90)
 const MAX_ANGLE = deg_to_rad(-2)
 const P_GAIN = 500000.0
@@ -36,23 +36,23 @@ func _physics_process(delta):
 		arm.angular_velocity *= 0.9  # Add damping to prevent infinite spinning
 
 	var error = 0.0
-	print("rotation = ", rad_to_deg(arm.rotation))
+	#print("rotation = ", rad_to_deg(arm.rotation))
 	if arm.rotation < MIN_ANGLE:
-		print("ANGLE TOO LOW")
+		#print("ANGLE TOO LOW")
 		error = MIN_ANGLE - arm.rotation
 		#arm.apply_torque(10000)
 		#arm.rotation = MIN_ANGLE
 	elif arm.rotation > MAX_ANGLE:
-		print("ANGLE TOO HIGH")
+		#print("ANGLE TOO HIGH")
 		#arm.rotation = MAX_ANGLE
 		#arm.apply_torque(-10000)
 		error = MAX_ANGLE - arm.rotation
 	#print(rad_to_deg(arm.rotation))
-	print("error = ", error)
+	#print("error = ", error)
 	# PD Control Calculation
 	var p_term = error * P_GAIN  # Proportional term (stronger correction if farther)
 	var d_term = ((error - previous_error) / delta) * D_GAIN  # Derivative term (damping)
 	var torque = p_term + d_term
-	print("torque = ", torque)
+	#print("torque = ", torque)
 	arm.apply_torque(p_term + d_term)
 	previous_error = error
