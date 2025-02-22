120197b7e651a6139a087172b0389a6accdd9e5f
Courtland Crouchet <CrouchetCourtland@gmail.com>
2025-02-22 17:51:40 -0500

    Restart Level Button

--- Scripts/main.gd
+++ Scripts/main.gd
@@ -1,11 +1,16 @@
 extends Node2D
+var restart_button
 
+func restart_scene():
+	get_tree().reload_current_scene()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass
+	restart_button = $HUD/Node/RestartButton
+	restart_button.connect("button_up", restart_scene)
+	
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	if Input.is_action_just_pressed("reset_level"):
-		get_tree().reload_current_scene()
+		restart_scene()
