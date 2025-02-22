2b2884a856328aa5b2ff7fee36670aa92854d850
Courtland Crouchet <CrouchetCourtland@gmail.com>
2025-02-21 21:07:54 -0500

    Saxophone

---
+++ Scripts/saxophone.gd
@@ -0,0 +1,9 @@
+extends RigidBody2D
+var explosion = preload("res://Scenes/explosion.tscn")
+
+func _on_body_entered(body: Node) -> void:
+	if body.is_in_group("Floor"):
+		var instance = explosion.instantiate()
+		instance.position = position
+		get_parent().add_child(instance)
+		queue_free()
